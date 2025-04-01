74aaf68949817e9e50151655da82d612f10c1f25
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-04-01 12:39:59 +0200

    Add project files.

---
+++ IndieGameJamAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IndieGameJamAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndieGameJamAPI.Data
+{
+    public class AppDbContext
+    {
+        public DbSet<Games> Games { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ IndieGameJamAPI/Model/Games.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IndieGameJamAPI.Model
+{
+    public class Games
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string DevelopmerGame { get; set; }
+    }
+}
